e78d1981cef9c0620c5138d048875373911993ef
ledxdeliveryflopp <korstim18@gmail.com>
2024-10-06 20:19:29 +0700

    moved search function to search widget, add cover image to track list

--- src/main/main_widget.py
+++ src/main/main_widget.py
@@ -17,73 +17,23 @@ class MainWidget(QtWidgets.QWidget):
         self.search_widget = None
         self.ui = MainWidgetUi()
         self.ui.setupUi(self)
-        self.init_model_and_search_widget()
+        self.init_search_widget()
         self.set_signals()
        
     @logger.catch
-    def init_model_and_search_widget(self) -> None:
-        """инициализация модели и виджета поиска"""
-        self.search_widget = SearchWidget()
-        self.model = QtGui.QStandardItemModel()
-        self.search_widget.ui.listView.setModel(self.model)
-        logger.info(f"{self.init_model_and_search_widget.__name__} - inited")
+    def init_search_widget(self) -> None:
+        """инициализация виджета поиска"""
+        self.search_widget = SearchWidget(music_widget=self.ui.music_widget)
+        logger.info(f"{self.init_search_widget.__name__} - inited")
     
     @logger.catch
     def set_signals(self) -> None:
         """Установка сигналов"""
-        self.search_widget.ui.search_input.textChanged.connect(self.set_music_to_list)
         self.ui.side_bar_widget.ui.search_button.clicked.connect(self.open_search_widget)
-        self.search_widget.ui.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.search_widget.ui.listView.pressed.connect(self.play_music_in_music_widget)
         logger.info(f"{self.set_signals.__name__} - inited")
-
-    @logger.catch
-    def play_music_in_music_widget(self, index: QModelIndex):
-        music_index = self.search_widget.ui.listView.model().itemFromIndex(index)
-        logger.info(f"play_music_in_music_widget index - {music_index.text()}")
-        self.ui.music_widget.play_music(music_index.text())
 
     @logger.catch
     def open_search_widget(self) -> None:
         self.ui.selection_widget.hide()
         self.ui.main_window_layout = QVBoxLayout(self.search_widget)
         self.ui.gridLayout_2.addWidget(self.search_widget, 1, 1, 1, 1)
-
-    @logger.catch
-    def search_music(self, query: str) -> None:
-        """Поиск музыки по автору и названию"""
-        try:
-            request = httpx.get(f"http://127.0.0.1:7000/music/find_music/?author_username="
-                                f"{query}&music_title={query}")
-            request_data = request.json()
-            return request_data
-        except Exception as exception:
-            logger.error(f"search_music error - {exception}")
-            return None
-
-    @logger.catch
-    def delete_duplicate(self, title: str) -> None:
-        """Удаление дубликатов"""
-        item = self.model.findItems(title)
-        if len(item) > 1:
-            row = item[1].row()
-            self.model.removeRow(row)
-
-    @logger.catch
-    def set_music_to_list(self, query) -> None:
-        """Добавить музыку в список"""
-        data = self.search_music(query)
-        try:
-            for music_info in data:
-                cover = music_info.get("cover_url")
-                title = music_info.get("title")
-                item = QtGui.QStandardItem(title)
-                self.model.appendRow(item)
-                self.delete_duplicate(title)
-        except TypeError:
-            logger.info(f"TypeError set music list exception")
-            if self.search_widget.ui.search_input.text() == "":
-                row_count = self.model.rowCount()
-                self.model.removeRows(0, row_count)
-            else:
-                pass
--- src/search/widget.py
+++ src/search/widget.py
@@ -1,12 +1,108 @@
-from PySide6 import QtWidgets
+from functools import partial
 
+import httpx
+import requests
+from PySide6 import QtWidgets, QtGui
+from PySide6.QtCore import QModelIndex, QSize
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtWidgets import QAbstractItemView
+from loguru import logger
+
+from src.music.widget import MusicWidget
 from src.search.widget_ui import SearchWidgetUi
+from src.settings.thread_manager import ThreadManager
 
 
-class SearchWidget(QtWidgets.QWidget):
+class SearchWidget(QtWidgets.QWidget, ThreadManager):
     """Виджет поиска музыки"""
 
-    def __init__(self):
+    def __init__(self, music_widget: MusicWidget):
         super().__init__()
+        self.model = None
+        self.music_widget = music_widget
         self.ui = SearchWidgetUi()
         self.ui.setupUi(self)
+        self.init_item_model()
+        self.set_signals()
+        self.cover_pixmap = QPixmap()
+
+    @logger.catch
+    def init_item_model(self) -> None:
+        """Инициализация  модели для списка"""
+        self.model = QtGui.QStandardItemModel()
+        self.ui.listView.setIconSize(QSize(50, 50))
+        self.ui.listView.setModel(self.model)
+
+    @logger.catch
+    def set_signals(self) -> None:
+        """Установка сигналов"""
+        self.ui.search_input.textChanged.connect(self.set_music_list_in_thread)
+        self.ui.listView.setSpacing(5)
+        self.ui.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.ui.listView.pressed.connect(self.play_music_in_music_widget)
+
+    @logger.catch
+    def play_music_in_music_widget(self, index: QModelIndex) -> None:
+        music_data = self.ui.listView.model().itemFromIndex(index)
+        music_split = music_data.text().split("\n")
+        music_title = music_split[0]
+        logger.info(f"play_music_in_music_widget index - {music_split}")
+        self.music_widget.play_music(music_title)
+
+    @logger.catch
+    def set_music_list_in_thread(self, query: str) -> None:
+        """Запуск функции добавления музыки в список в отдельном потоке"""
+        self.thread_manager.start(partial(self.set_music_to_list, query))
+
+    @logger.catch
+    def search_music(self, query: str) -> list | None:
+        """Поиск музыки по автору и названию"""
+        try:
+            request = httpx.get(f"http://127.0.0.1:7000/music/find_music/?author_username="
+                                f"{query}&music_title={query}")
+            request_data = request.json()
+            return request_data
+        except Exception as exception:
+            logger.error(f"search_music error - {exception}")
+            return None
+
+    @logger.catch
+    def set_cover_to_music_in_list(self, cover_url: str) -> QIcon:
+        """Установить обложки для мызыки в списке"""
+        cover_data = requests.get(cover_url)
+        pixmap = QPixmap()
+        pixmap.loadFromData(cover_data.content)
+        icon = QtGui.QIcon(pixmap)
+        return icon
+
+    @logger.catch
+    def set_music_to_list(self, query) -> None:
+        """Добавить музыку в список"""
+        data = self.search_music(query)
+        try:
+            for music_info in data:
+                cover_url = music_info.get("cover_url")
+                title = music_info.get("title")
+                author_data = music_info.get("owner")
+                author = author_data.get("username")
+                title_author = f"{title}\n{author}"
+                item = QtGui.QStandardItem(title_author)
+                icon = self.set_cover_to_music_in_list(cover_url)
+                item.setIcon(icon)
+                self.model.appendRow(item)
+                self.delete_duplicate(title_author)
+        except Exception as exc:
+            logger.info(f"{self.set_music_to_list.__name__} exception - {exc}")
+            if self.ui.search_input.text() == "":
+                row_count = self.model.rowCount()
+                self.model.removeRows(0, row_count)
+            else:
+                pass
+
+    @logger.catch
+    def delete_duplicate(self, title_author: str) -> None:
+        """Удаление дубликатов"""
+        item = self.model.findItems(title_author)
+        if len(item) > 1:
+            row = item[1].row()
+            self.model.removeRow(row)
