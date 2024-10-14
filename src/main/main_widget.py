e633c716cf2d2e8a85f64dd3a91ea2a8829f6eaa
ledxdeliveryflopp <korstim18@gmail.com>
2024-10-14 16:50:54 +0700

    add token check response when app start

--- src/main/main_widget.py
+++ src/main/main_widget.py
@@ -1,8 +1,10 @@
+import httpx
 from PySide6 import QtWidgets
 from loguru import logger
 
 from src.main.widget_ui import Ui_MainWindow
 from src.settings.config import ini_settings
+from src.settings.settings import settings
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -15,10 +17,24 @@ class MainWidget(QtWidgets.QWidget):
         self.check_authorization()
 
     @logger.catch
+    def check_token_response(self, token: str) -> bool:
+        """Запрос на эндопинт проверки токена"""
+        try:
+            response = httpx.post(f"{settings.api_settings.api_url}check_token/?token={token}")
+            if response.status_code == 200:
+                response_json = response.json()
+                data = response_json.get("detail")
+                return data
+        except Exception as e:
+            logger.error(f"{self.check_token_response.__name__} error - {e}")
+            return False
+
+    @logger.catch
     def check_authorization(self) -> None:
         """Проверка авторизации"""
         token_in_config = ini_settings.check_auth_token_section()
-        if token_in_config is True:
+        response = self.check_token_response(token_in_config)
+        if response is True:
             pass
         else:
             self.open_authorization_widget()
